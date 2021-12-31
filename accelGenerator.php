8eeb70a0a9ff3b003e0931c7adc849e10a6daa48
Muhammad Qisthi Salikin bin Samsudin <74824168+muhammadqisthisalikin@users.noreply.github.com>
2021-12-31 14:34:56 +0800

    Update accelGenerator.php

--- accelGenerator.php
+++ accelGenerator.php
@@ -1,3 +1,4 @@
 <?php
-echo '{"xA":-112, "yA":-112, "zA":-112, "roll":-26 ,"pitch":-105 ,"yaw":81}';
+//echo '{"xA":-112, "yA":-112, "zA":-112, "roll":-26 ,"pitch":-105 ,"yaw":81}';
+echo '{"xA":-25140.386012294526, "yA":87, "zA":"Connected", "roll":0 ,"pitch":57 ,"yaw":686,"a":"Alert"}';
 ?>
